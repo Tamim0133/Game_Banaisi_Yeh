b748a72e8114706c2abd8afad2fb67f87145b00e
Tamim0133 <tamimdewanandnobody@gmail.com>
2023-11-10 09:06:07 +0600

    Second Commit

---
+++ Game/Window_Create.cpp
@@ -0,0 +1,18 @@
+//
+//  Window_Create.cpp
+//  Game_Final
+//
+//  Created by Tamim Dewan on 10/11/23.
+//
+
+#include "Window_Create.hpp"
+
+
+SDL_Window * Window_Create :: create_window(string s, int width , int height){
+    SDL_Window * win = SDL_CreateWindow(s.c_str(),SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, width, height, 0);
+    
+    if(win == NULL){
+        printf( "Window could not initialize! SDL_Error: %s\n", SDL_GetError());
+    }
+    return win;
+}
---
+++ Game/game.hpp
@@ -0,0 +1,28 @@
+//
+//  game.hpp
+//  Game
+//
+//  Created by Tamim Dewan on 10/11/23.
+//
+
+#ifndef game_hpp
+#define game_hpp
+
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include <SDL2_ttf/SDL_ttf.h>
+#include <SDL2_mixer/SDL_mixer.h>
+#include <iostream>
+#include "death.hpp"
+#include "Sound.hpp"
+using namespace std;
+
+
+struct Game {
+public:
+    Game();
+private:
+    
+};
+
+#endif /* game_hpp */
